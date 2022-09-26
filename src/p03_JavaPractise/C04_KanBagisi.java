edc34f6b9a1408f02739dae5bfdf350dcafc11e2
SunYasmin <110902506+SunYasmin@users.noreply.github.com>
2022-09-26 08:53:46 +0300

    Update C04_KanBagisi.java

--- src/p03_JavaPractise/C04_KanBagisi.java
+++ src/p03_JavaPractise/C04_KanBagisi.java
@@ -1,5 +1,33 @@
 package p03_JavaPractise;public class C04_KanBagisi {
   
-  
+  public static void main(String[] args) {
+
+        /*
+Problem tanimi :
+Kullanicidan  yasini ve kilosunu alaliniz
+18 yasindan kucuk ise kan bagisi yapamaz
+18 yasindann buyuk ve 50 kilo dan hafif ise kan bagisi yapamaz.
+18 yasindan buyuk ve 50 kilodan agir ise kan bagisi yapabilir.
+ */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen yasinizi giriniz : ");
+        int yas = scan.nextInt();
+
+        System.out.println("Lutfen agirliginizi kg olarak giriniz : ");
+        int agirlik = scan.nextInt();
+
+        if (yas>0 && yas<18){   // (yas>0 && yas<18) olurrrrr
+            System.out.println("Yasi 18'den kucuk olanlar kan bagisi yapamaz");
+        } else if (yas >= 18) { //nested if else if e yeni if actik
+            if (agirlik>0 && agirlik<50){
+                System.out.println("Agirliginiz 50'den kücük oldugu icin kan bagisi yapamazsiniz");
+            } else if (agirlik >= 50) {
+                System.out.println("Kan bagisi yapabilirsiniz");
+            }else {System.out.println("Yanlis deger girdiniz");//buna gerek kalmadi
+
+            }
+
+
+        }
   
 }
